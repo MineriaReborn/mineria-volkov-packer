c474c06e1762da8054a65c524fb5bf5580d92194
Clément <cb1d51ed8ceb6ceff21750fb000f54@gmail.com>
2025-05-29 21:27:50 +1000

    update - v2.0.5

--- src/main/java/fr/mineria/volkov/VolkovCLI.java
+++ src/main/java/fr/mineria/volkov/VolkovCLI.java
@@ -6,7 +6,7 @@ import picocli.CommandLine;
 @CommandLine.Command(
     name = "volkov",
     mixinStandardHelpOptions = true,
-    version = "Volkov 2.0.3",
+    version = "Volkov 2.0.5",
     subcommands = {PackCommand.class, UnpackCommand.class}
 )
 public class VolkovCLI implements Runnable {
